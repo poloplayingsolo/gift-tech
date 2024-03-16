738a8a67c865d09652d6a1d4e58da6ffdb6e0880
Dmytro Pintak <dmytro.pintak@gmail.com>
2024-03-16 21:08:39 +0000

    feat: kidle removed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,15 @@
 import { useAccount } from "wagmi";
 import { Link } from 'wouter'
-import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-
-import { Button } from "@/components/ui/button";
 
 export function HomePage() {
   const account = useAccount();
-  const { login, isAuthenticated, logout, user } = useKindeAuth();
 
   return (
     <div className="p-5">
       <div>{account.address ?? "No address, connect wallet"}</div>
-      <div>User: {JSON.stringify(user)}</div>
       <div className="mt-5">
         <ConnectButton />
-      </div>
-      <div className="mt-5">
-        {isAuthenticated ? (
-          <div>
-            <div>Authenticated</div>
-            <Button onClick={() => logout()}>Log out</Button>
-          </div>
-        ) : (
-          <Button onClick={() => login()}>Sign in with X</Button>
-        )}
       </div>
       <div className="mt-5">
         <Link href="/other">Link to other page</Link>
